ec7214971d33853b9cfce0102cb52f80c6eba28c
tuliopaim <tutpaim@gmail.com>
2021-04-08 22:04:48 -0300

    Create AddDTOs to receive data from controller

---
+++ UoW.Api/DTOs/AddClassDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UoW.Api.DTOs
+{
+    public class AddClassDTO
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string TeacherName { get; set; }
+    }
+}
---
+++ UoW.Api/DTOs/AddStudentDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UoW.Api.DTOs
+{
+    public class AddStudentDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
