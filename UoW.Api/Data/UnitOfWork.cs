4d8848d5ab18eca8dac6bb9947ed453dbaf5e528
tuliopaim <tutpaim@gmail.com>
2021-04-11 15:54:09 -0300

    Create properties for the repositories in the UoW class

--- UoW.Api/Data/UnitOfWork.cs
+++ UoW.Api/Data/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using UoW.Api.Data.Repositories;
 using UoW.Api.Domain.Interfaces;
 
 namespace UoW.Api.Data
@@ -8,10 +9,19 @@ namespace UoW.Api.Data
     {
         private readonly ApplicationContext _context;
 
+        private IStudentRepository _studentRepository;
+        private IClassRepository _classRepository;
+
         public UnitOfWork(ApplicationContext context)
         {
             _context = context;
         }
+        
+        public IStudentRepository StudentRepository => 
+            _studentRepository ??= new StudentRepository(_context);
+
+        public IClassRepository ClassRepository =>
+            _classRepository ??= new ClassRepository(_context);
 
         public async Task CommitAsync()
         {
--- UoW.Api/Domain/Interfaces/IUnitOfWork.cs
+++ UoW.Api/Domain/Interfaces/IUnitOfWork.cs
@@ -5,5 +5,8 @@ namespace UoW.Api.Domain.Interfaces
     public interface IUnitOfWork
     {
         Task CommitAsync();
+
+        IStudentRepository StudentRepository { get;  }
+        IClassRepository ClassRepository { get; }
     }
 }
