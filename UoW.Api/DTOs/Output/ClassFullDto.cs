9bfd80d33ffe2f0001f18d344b2edc5aef703620
tuliopaim <tutpaim@gmail.com>
2021-04-11 22:09:09 -0300

    Separate standard and full output dtos

--- UoW.Api/DTOs/Output/ClassFullDto.cs
+++ UoW.Api/DTOs/Output/ClassFullDto.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using UoW.Api.Domain.Entities;
 
 namespace UoW.Api.DTOs.Output
 {
-    public struct ClassDto
+    public struct ClassFullDto
     {
         public Guid Id { get; set; }
         public long Code { get; set; }
         public string Name { get; set; }
         public int Year { get; set; }
         public string TeacherName { get; set; }
+        public int StudentsTotal { get; set; }
+
+        public IEnumerable<StudentDto> Students { get; set; }
     }
 }
--- UoW.Api/DTOs/Output/StudentFullDto.cs
+++ UoW.Api/DTOs/Output/StudentFullDto.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using UoW.Api.Domain.Entities;
 
 namespace UoW.Api.DTOs.Output
 {
-    public struct StudentDto
+    public struct StudentFullDto
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime BrithDate { get; set; }
+
+        public IEnumerable<ClassDto> Classes { get; set; }
     }
 }
